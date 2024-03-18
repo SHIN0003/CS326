2ee2a4c1f95298cdf9fed337faf2dccb11fed95b
Andrew Tai <andrewtai@umass.edu>
2024-03-18 14:42:37 -0400

    initbag

--- hw02-rack-and-render/src/game.js
+++ hw02-rack-and-render/src/game.js
@@ -32,8 +32,22 @@ export class Game {
   #initBag() {
     // TASK #1: Implement the initBag method
     const bag = [];
-    
-    return bag;
+    // 1 point: E ×12, A ×9, I ×9, O ×8, N ×6, R ×6, T ×6, L ×4, S ×4, U ×4
+    // 2 points: D ×4, G ×3
+    // 3 points: B ×2, C ×2, M ×2, P ×2
+    // 4 points: F ×2, H ×2, V ×2, W ×2, Y ×2
+    // 5 points: K ×1
+    // 8 points: J ×1, X ×1
+    // 10 points: Q ×1, Z ×1
+    let letters = {E: 12, A: 9, I: 9, O: 8, N: 6, R: 6, T: 6, L: 4, S: 4, U: 4, 
+      D: 4, G: 3, B: 2, C: 2, M: 2, P: 2, F: 2, 
+      H: 2, V: 2, W: 2, Y: 2, K: 1, J: 1, X: 1, Q: 1, Z: 1};
+    for (let letter in letters) {
+      for (let i = 0; i < letters[letter]; i++) {
+        bag.push(letter);
+      }
+    }
+    return shuffle(bag);
   }
 
   #initGrid() {
